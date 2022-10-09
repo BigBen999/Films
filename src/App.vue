3be6e17b0880c4e8c5e0da75c812093298ad913a
David <d.barcia9@gmail.com>
2022-10-09 13:22:18 +0200

    project front

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,58 @@
 <template>
   <div>
     <h1>Movies</h1>
+    <FilterByStatus />
+    <FilterByName />
     <ListCharacter />
   </div>
 </template>
 
 
 <script>
-import ListCharacter from './components/ListCharacter.vue';
+import ListCharacter from './components/ListCharacter.vue'
+import FilterByStatus from './components/FilterByStatus.vue';
+import FilterByName from './components/FilterByName.vue';
   export default {
     name: 'App',
     components: {
-    ListCharacter
+    ListCharacter,
+    FilterByStatus,
+    FilterByName
 }
     }
 
 </script>
 
 <style lang="scss">
+  :root {
+    --background-body: aquamarine;
+    --background-card: #3c3e44;
+    --text-white: white;
+    --text-gray: gray;
+    --text-orange: orange;
+  }
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  
+  }
+  body {
+    background-color: var(--background-body);
+    color: var(--text-white);
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  }
+  h1{
+    margin-bottom: 3rem;
+    text-align: center;
+  }
+  .container {
+    width: 980px;
+    max-width: 90%;
+    margin: 5rem auto;
+  }
+  img {
+    width: 100%;
+  }
 
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -9,23 +9,58 @@ export default createStore({
   },
   mutations: {
     setCharacters(state, payload) {
-      state.characters= payload
+      state.characters = payload
   },
   setCharactersFilter(state, payload) {
-    state.charactersFilter= payload
+    state.charactersFilter = payload
   }
 },
   actions: {
   async getCharacters({commit}) {
       try {
-        const response = await fetch('')
+        const response = await fetch('https://rickandmortyapi.com/api/character')
         const data = await response.json()
-        console.log(data)
+        commit ('setCharacters', data.results)
+        commit ('setCharactersFilter', data.results)
       } catch(error) {
           console.error(error)
       }
-    }
+    },
+    filterByStatus({ commit, state}, status) {
+      const results = state.characters.filter((character) => {
+        return character.status.includes(status)
+      })
+      commit ('setCharactersFilter', results)
+    },
+    filterByName({commit, state}, name) {
+      const formatName = name.toLowerCase()
+      const results = state.characters.filter((character) => {
+      const characterName = character.name.toLowerCase()
+
+      if(characterName.includes(formatName)) {
+        return character 
+      }
+
+    })
+    commit ('setCharactersFilter', results)
+  }
   },
   modules: {
   }
 })
+
+/*export default {
+  name: "characters",
+  data() {
+    return {
+      characterDataList: []
+    };
+  },
+  methods: {
+    getWeatherData() {
+      fetch("../assets/characters.json")
+        .then(response => response.json())
+        .then(data => (this.charactersDataList = data));
+    }
+  }
+};*/
